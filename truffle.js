90576a1f53684290dc0fca85b76917b947b903b5
mushroom0117 <v860117@gmail.com>
2019-08-18 22:38:12 +0800

    Fix truffle config

--- truffle.js
+++ truffle.js
@@ -9,80 +9,67 @@ module.exports = {
       host: "192.168.56.3",
       port: 20010,
       network_id: "*",
-      gas: 200000000,
-      type: "quorum"
+      gas: 200000000
     },
     cb: {
       host: "192.168.56.4",
       port: 20010,
-      network_id: "*",
-      type: "quorum"
+      network_id: "*"
     },
     a: {
       host: "192.168.56.5",
       port: 20010,
-      network_id: "*",
-      type: "quorum"
+      network_id: "*"
     },
     b: {
       host: "192.168.56.6",
       port: 20010,
-      network_id: "*",
-      type: "quorum"
+      network_id: "*"
     },
     c: {
       host: "192.168.56.7",
       port: 20010,
-      network_id: "*",
-      type: "quorum"
+      network_id: "*"
     },
     d: {
       host: "192.168.56.8",
       port: 20010,
-      network_id: "*",
-      type: "quorum"
+      network_id: "*"
     },
     e: {
       host: "192.168.56.9",
       port: 20010,
-      network_id: "*",
-      type: "quorum"
+      network_id: "*"
     },
     f: {
       host: "192.168.56.10",
       port: 20010,
-      network_id: "*",
-      type: "quorum"
+      network_id: "*"
     },
     g: {
       host: "192.168.56.11",
       port: 20010,
-      network_id: "*",
-      type: "quorum"
+      network_id: "*"
     },
     h: {
       host: "192.168.56.12",
       port: 20010,
-      network_id: "*",
-      type: "quorum"
+      network_id: "*"
     },
     i: {
       host: "192.168.56.13",
       port: 20010,
-      network_id: "*",
-      type: "quorum"
+      network_id: "*"
     },
     j: {
       host: "192.168.56.14",
       port: 20010,
-      network_id: "*",
-      type: "quorum"
+      network_id: "*"
     },
     k: {
       host: "192.168.56.15",
       port: 20010,
-      network_id: "*",
-      type: "quorum"
+      network_id: "*"
     }
   },
   compilers: {
